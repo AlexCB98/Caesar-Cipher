730c8eefc449201f6a672e89bbc1c73ecf8e8213
Alexandru <lexyus22@gmail.com>
2026-04-18 10:33:18 +0200

    Improve user prompts and output formatting.

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from ascii_art import logo
+print(logo)
 
-def caesar(original_text, shift_amount, encode_or_decode):
-    output_text = ""
+lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+digits = ['0','1','2','3','4','5','6','7','8','9']
 
-    if encode_or_decode == "decode":
+def caesar(encode_or_decode, original_text, shift_amount):
+
+    cipher_text = ''
+
+    if encode_or_decode == 'decode':
         shift_amount *= -1
 
     for letter in original_text:
-        if letter not in alphabet:
-            output_text += letter
+        if letter in lowercase:
+            shifted_position = lowercase.index(letter) + shift_amount
+            shifted_position %= len(lowercase)
+            cipher_text += lowercase[shifted_position]
+        elif letter in uppercase:
+            shifted_position = uppercase.index(letter) + shift_amount
+            shifted_position %= len(uppercase)
+            cipher_text += uppercase[shifted_position]
+        elif letter in digits:
+            shifted_position = digits.index(letter) + shift_amount
+            shifted_position %= len(digits)
+            cipher_text += digits[shifted_position]
         else:
-            shifted_position = alphabet.index(letter) + shift_amount
-            shifted_position %= len(alphabet)
-            output_text += alphabet[shifted_position]
-    print(f"Here is the {encode_or_decode}d result: {output_text}")
+            cipher_text += letter
+    print(f'Your cipher text is: {cipher_text}')
 
-try_again = 'yes'
-while try_again == 'yes':
-     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-     text = input("Type your message:\n").lower()
-     shift = int(input("Type the shift number:\n"))
+again = 'yes'
 
-     caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
-     try_again = input('Type "yes" if you want to repeat or "no" to exit:\n ').lower()
+while again == 'yes':
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n")
+    shift = int(input("Type the shift number:\n"))
+    caesar(encode_or_decode = direction, original_text = text, shift_amount = shift)
+    again = input('Type "yes" if you want to continue or "no" if you want to stop:\n').lower()
 
-print('Goodbye!')
 
+print("Thanks for using Caesar Cipher. Goodbye! ^^")
